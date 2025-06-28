987df4d15f7ad2b5e0e1c5c6eaf96f79ea667e9b
Lau02x <castrorodriguez2@gmail.com>
2025-06-28 02:04:53 -0300

    Ejercicio 4, aplico el substring y muestro cada caracter del texto concatenado

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,13 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 string texto1;
 string texto2;
 string textoconcatenado;
 Console.WriteLine("Dar una cadena de texto:");
 texto1 =Console.ReadLine();
-int longitud = texto.Length;
+int longitud = texto1.Length;
 Console.WriteLine("La longitud del texto es de: " + longitud);
 
 Console.WriteLine("Dar otra cadena de texto para concatenar con la anterior:");
 texto2 = Console.ReadLine();
-textoconcatenado = Concat(texto1, texto2);
+textoconcatenado = string.Concat(texto1, texto2);
 Console.WriteLine(textoconcatenado);
+Console.WriteLine("Muestro la subcadena desde el indice 7 del texto concatenado : ");
+Console.WriteLine(textoconcatenado.Substring(7));
+
+foreach (var c in textoconcatenado)
+{
+    Console.WriteLine(c);
+    
+}
